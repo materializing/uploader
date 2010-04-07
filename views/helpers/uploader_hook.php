61c10b8d8fce296130319aa1a1483131a7039c33
ryuring <ryuring@e615f810-9de0-4b00-b314-8e5587af3f6a>
2010-04-07 07:22:37 +0000

    UploaderHookHelper コード整理
    git-svn-id: http://svn.e-catchup.jp/baseruploader/trunk@13 e615f810-9de0-4b00-b314-8e5587af3f6a

--- views/helpers/uploader_hook.php
+++ views/helpers/uploader_hook.php
@@ -19,99 +19,124 @@
  * @lastmodified	$Date$
  * @license			http://basercms.net/license/index.html
  */
-class UploaderHookHelper extends AppHelper{
+class UploaderHookHelper extends AppHelper {
 /**
  * afterLayout
+ * 
+ * @return void
+ * @access pubic
  */
-	function afterLayout(){
+	function afterLayout() {
 
 		$view =& ClassRegistry::getObject('view');
 
-        if($view){
-            if(isset($view->loaded['ckeditor'])){
-                if(preg_match_all("/var\s*?(editor_[a-z0-9]*?)\s*?=\s*?CKEDITOR.replace/s",$view->output,$matches)){
-                    $script = '<link rel="stylesheet" type="text/css" href="'.$this->webroot('/uploader/css/uploader.css').'" />';
-                    $script .= "<script type=\"text/javascript\">var baseUrl ='".$this->base.'/'."';</script>";
-                    $script .= '<script type="text/javascript" src="'.$this->webroot('/uploader/js/ckeditor_uploader.js').'"></script>';
-                    $view->output = str_replace('</body>',$script.'</body>',$view->output);
-                    foreach($matches[1] as $key => $match){
-                        $script = str_replace('EDITOR_NAME',$match,$this->getCkeditorUploaderScript());
-                        $pattern = "/(<script type=\"text\/javascript\">.*?var\s*?".$match."\s*?=\s*?CKEDITOR.replace.*?)\/\/\]\]>\n*?<\/script>/s";
-                        $view->output = preg_replace($pattern,"$1".$script,$view->output);
-                    }
-                    //$view->output = str_replace('</body>',$script.'</body>',$view->output);
-                    $pattern = "/(CKEDITOR\.replace.*?\"toolbar\".*?)\"Image\"(.*?);/is";
-                    $view->output = preg_replace($pattern,"$1".'"BaserImage"'."$2;",$view->output);
-                }
-            }elseif(!empty($view->params['prefix']) && $view->params['prefix'] == 'mobile'){
-                // モバイル画像に差し替える
-                $aMatch = "/<a([^>]*?)href=\"([^>]*?)\"([^>]*?)><img([^>]*?)\/><\/a>/is";
-                $imgMatch = "/<img([^>]*?)src=\"([^>]*?)\"([^>]*?)\/>/is";
-                $view->output = preg_replace_callback($aMatch,array($this,"mobileImageAnchorReplace"),$view->output);
-                $view->output = preg_replace_callback($imgMatch,array($this,"mobileImageReplace"),$view->output);
-            }
+		if($view) {
 
-        }
+			if(isset($view->loaded['ckeditor'])) {
+
+				if(preg_match_all("/var\s*?(editor_[a-z0-9]*?)\s*?=\s*?CKEDITOR.replace/s",$view->output,$matches)) {
+
+					$script = '<link rel="stylesheet" type="text/css" href="'.$this->webroot('/uploader/css/uploader.css').'" />';
+					$script .= "<script type=\"text/javascript\">var baseUrl ='".$this->base.'/'."';</script>";
+					$script .= '<script type="text/javascript" src="'.$this->webroot('/uploader/js/ckeditor_uploader.js').'"></script>';
+					$view->output = str_replace('</body>',$script.'</body>',$view->output);
+
+					foreach($matches[1] as $key => $match) {
+						$script = str_replace('EDITOR_NAME',$match,$this->__getCkeditorUploaderScript());
+						$pattern = "/(<script type=\"text\/javascript\">.*?var\s*?".$match."\s*?=\s*?CKEDITOR.replace.*?)\/\/\]\]>\n*?<\/script>/s";
+						$view->output = preg_replace($pattern,"$1".$script,$view->output);
+					}
+
+					//$view->output = str_replace('</body>',$script.'</body>',$view->output);
+					$pattern = "/(CKEDITOR\.replace.*?\"toolbar\".*?)\"Image\"(.*?);/is";
+					$view->output = preg_replace($pattern,"$1".'"BaserImage"'."$2;",$view->output);
+
+				}
+
+			} elseif (!empty($view->params['prefix']) && $view->params['prefix'] == 'mobile') {
+
+				// モバイル画像に差し替える
+				$aMatch = "/<a([^>]*?)href=\"([^>]*?)\"([^>]*?)><img([^>]*?)\/><\/a>/is";
+				$imgMatch = "/<img([^>]*?)src=\"([^>]*?)\"([^>]*?)\/>/is";
+				$view->output = preg_replace_callback($aMatch,array($this,"__mobileImageAnchorReplace"),$view->output);
+				$view->output = preg_replace_callback($imgMatch,array($this,"__mobileImageReplace"),$view->output);
+
+			}
+
+		}
 
 	}
 /**
  * 画像タグをモバイル用に置き換える
+ * 
  * @param array $matches
  * @return string
+ * @access private
  */
-    function mobileImageReplace($matches){
-        $url = $matches[2];
-        $pathinfo = pathinfo($url);
-        if(!isset($pathinfo['extension'])){
-            return $matches[0];
-        }
-        $url = str_replace('__small','',$url);
-        $url = str_replace('__midium','',$url);
-        $url = str_replace('__large','',$url);
-        $basename = basename($url,'.'.$pathinfo['extension']);
-        $_url = 'files'.DS.'uploads'.DS.$basename.'__mobile_small.'.$pathinfo['extension'];
-        // TODO uploads固定となってしまっているのでmodelから取得するようにする
-        $path = WWW_ROOT.$_url;
-        if(file_exists($path)){
-            return '<img'.$matches[1].'src="'.$this->webroot($_url).'"'.$matches[3].'/>';
-        }else{
-            return $matches[0];
-        }
+	function __mobileImageReplace($matches) {
+		
+		$url = $matches[2];
+		$pathinfo = pathinfo($url);
 
-    }
+		if(!isset($pathinfo['extension'])) {
+			return $matches[0];
+		}
+
+		$url = str_replace('__small','',$url);
+		$url = str_replace('__midium','',$url);
+		$url = str_replace('__large','',$url);
+		$basename = basename($url,'.'.$pathinfo['extension']);
+		$_url = 'files'.DS.'uploads'.DS.$basename.'__mobile_small.'.$pathinfo['extension'];
+		// TODO uploads固定となってしまっているのでmodelから取得するようにする
+		$path = WWW_ROOT.$_url;
+
+		if(file_exists($path)) {
+			return '<img'.$matches[1].'src="'.$this->webroot($_url).'"'.$matches[3].'/>';
+		}else {
+			return $matches[0];
+		}
+
+	}
 /**
  * アンカータグのリンク先が画像のものをモバイル用に置き換える
+ *
  * @param array $matches
- * @param string
- */
-    function mobileImageAnchorReplace($matches){
-
-        $url = $matches[2];
-        $pathinfo = pathinfo($url);
-        if(!isset($pathinfo['extension'])){
-            return $matches[0];
-        }
-        $url = str_replace('__small','',$url);
-        $url = str_replace('__midium','',$url);
-        $url = str_replace('__large','',$url);
-        $basename = basename($url,'.'.$pathinfo['extension']);
-        $_url = 'files'.DS.'uploads'.DS.$basename.'__mobile_large.'.$pathinfo['extension'];
-        // TODO uploads固定となってしまっているのでmodelから取得するようにする
-        $path = WWW_ROOT.$_url;
-        if(file_exists($path)){
-            return '<a'.$matches[1].'href="'.$this->webroot($_url).'"'.$matches[3].'><img'.$matches[4].'/></a>';
-        }else{
-            return $matches[0];
-        }
-
-    }
-/**
- * CKEditorのアップローダーを組み込む為のスクリプト返す
  * @return string
+ * @access private
  */
-    function getCkeditorUploaderScript(){
+	function __mobileImageAnchorReplace($matches) {
 
-        return <<< DOC_END
+		$url = $matches[2];
+		$pathinfo = pathinfo($url);
+
+		if(!isset($pathinfo['extension'])) {
+			return $matches[0];
+		}
+
+		$url = str_replace('__small','',$url);
+		$url = str_replace('__midium','',$url);
+		$url = str_replace('__large','',$url);
+		$basename = basename($url,'.'.$pathinfo['extension']);
+		$_url = 'files'.DS.'uploads'.DS.$basename.'__mobile_large.'.$pathinfo['extension'];
+		// TODO uploads固定となってしまっているのでmodelから取得するようにする
+		$path = WWW_ROOT.$_url;
+		
+		if(file_exists($path)) {
+			return '<a'.$matches[1].'href="'.$this->webroot($_url).'"'.$matches[3].'><img'.$matches[4].'/></a>';
+		}else {
+			return $matches[0];
+		}
+
+	}
+/**
+ * CKEditorのアップローダーを組み込む為のJavascriptを返す
+ * 
+ * @return string
+ * @access private
+ */
+	function __getCkeditorUploaderScript() {
+
+		return <<< DOC_END
 EDITOR_NAME.on( 'pluginsLoaded', function( ev ) {
     // baserImageを開くコマンドを追加
     EDITOR_NAME.addCommand( 'baserImage', new CKEDITOR.dialogCommand( 'baserImageDialog' ) );
@@ -122,7 +147,7 @@ EDITOR_NAME.on( 'pluginsLoaded', function( ev ) {
 </script>
 DOC_END;
 
-    }
+	}
 
 }
 ?>
